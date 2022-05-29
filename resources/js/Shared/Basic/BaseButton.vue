90af4aa5b6292238a837d7e07f00d2876414cad4
bennyharvey <johnphilips1973@gmail.com>
2022-05-29 14:27:29 +0300

    add base button

---
+++ resources/js/Shared/Basic/BaseButton.vue
@@ -0,0 +1,29 @@
+<script setup>
+const props = defineProps({
+    text: {
+        type: String,
+        required: true,
+    },
+    type: {
+        type: String,
+        default: 'primary'
+    },
+    ucase: {
+        type: Boolean,
+        default: true
+    }
+})
+</script>
+
+<template>
+    <el-button :type=props.type class="basic-button">
+        {{props.ucase ? text.toUpperCase() : props.text}}
+        <slot/>
+    </el-button>
+</template>
+
+<style>
+.basic-button {
+    font-weight: 600;
+}
+</style>
