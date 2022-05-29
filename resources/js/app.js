a984bef836ae0aa5cbecd9d95ca74230219663d5
bennyharvey <johnphilips1973@gmail.com>
2022-05-29 12:39:22 +0300

    add global layout config

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,3 +1,4 @@
+
 require('./bootstrap');
 
 import { createApp, h } from 'vue'
@@ -7,6 +8,7 @@ import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import Equal from 'equal-vue'
 import 'equal-vue/dist/style.css'
+import Layout from "./Shared/Layout";
 
 import '../css/dark-theme-css-vars.css'
 import '../css/theme.css'
@@ -14,13 +16,19 @@ import '../css/theme.css'
 InertiaProgress.init()
 
 createInertiaApp({
-    resolve: name => require(`./Pages/${name}`),
+    resolve: name =>{
+        const page = require(`./Pages/${name}`).default
+        if (page.layout === undefined && !name.startsWith('Public/')) {
+            page.layout = Layout
+        }
+        return page
+    },
     setup({ el, App, props, plugin }) {
         const app = createApp({ render: () => h(App, props) })
             .use(plugin)
             .use(ElementPlus)
             .use(Equal)
-        app.config.globalProperties.$route = route
+        app.config.globalProperties.$route = route // loaded by "ziggy" library
         app.mount(el)
     }
 })
